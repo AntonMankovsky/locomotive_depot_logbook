407c486c46c4dd39426768d922f85244c919ace0
AntonMankovsky <mankovsky.anton@live.ru>
2022-07-10 07:27:11 +0300

    Initial commit.

---
+++ src/main/java/anton/mankovsky/locomotive_depot_logbook/LocomotiveDepotLogbookApplication.java
@@ -0,0 +1,13 @@
+package anton.mankovsky.locomotive_depot_logbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocomotiveDepotLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocomotiveDepotLogbookApplication.class, args);
+	}
+
+}
